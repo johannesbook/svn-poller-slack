872e74f9b8a10c7c6ce6f27722567505d0b011b0
johannesbook <joh@nnes.se>
2017-08-21 08:33:55 +0200

    added support for multi-file checkin

--- robbot.py
+++ robbot.py
@@ -1,17 +1,22 @@
 #hack to check last svn checkin, and post it to slack channel #altium-svn-updates
 #Johannes Book 2017
 
-pathToLocalSvn = '"c:/altium work/WalterSVN/"'  #Note - don't use backslash
-pathToDataFile = 'c:/temp/robbot.data'
+pathToLocalSvn = '"C:/robbot/svn checkout"'  #Note - don't use backslash
+pathToDataFile = 'C:/robbot/robbot.data'
 
-webhookUrl = 'https://hooks.slack.com/services/GET/THIS/LINK_FROM_SLACK_WEBHOOKS_API'
+webhookUrl = 'https://hooks.slack.com/services/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'
 
+import sys
 import re
 import time
 import json
 import requests
+from xml.dom import minidom
 from subprocess import check_output
+files = []
 
+print('Starting..')
+	
 while True:
 	#open last revision from file
 	file = open(pathToDataFile,"r")
@@ -28,32 +33,43 @@ while True:
 		newCommit = False
 		
 	if newCommit: #get commit details and post them to slack
-		match = re.search(r'<author>(.*)</author>.*action.*>(.*)</path>.*<msg>(.*)</msg>',output,re.DOTALL) #I need to learn XML decoding, I know..
+		#match = re.search(r'revision=\"(.*)\">.*<author>(.*)</author>.*action.*>(.*)</path>.*<msg>(.*)</msg>',output,re.DOTALL) #I need to learn XML decoding, I know..
+		match = re.search(r'revision=\"(.*)\">.*<author>(.*)</author>.*<paths>(.*)</paths>.*<msg>(.*)</msg>',output,re.DOTALL) #I need to learn XML decoding, I know..
 		if match:
-			who = match.group(1)
-			path = match.group(2)
-			what = match.group(3)
-			match2 = re.search(r'/.*/.*/(.*)$',path,re.M|re.DOTALL)	#get file name
-			if match2:
-				file = match2.group(1)
-			else:
-				file = ""
-			match3 = re.search(r'/([A-Z]{3,20}?).*/(.*)$',path,re.M|re.DOTALL) #get project name
-			if match3:
-				project = match3.group(1)
-			else:
-				project = ""
+			rev = match.group(1)
+			who = match.group(2)
+			paths = match.group(3)
+			what = match.group(4)
+			
+			#parse all file paths
+			xmldoc = minidom.parseString(output)
+			itemlist = xmldoc.getElementsByTagName('path')
+			for s in itemlist:
+				#print(s.firstChild.nodeValue)
+				files.append(s.firstChild.nodeValue)
+
+			for i, item in enumerate(files):
+				match2 = re.search(r'/.*/([A-Z]{3,20}?).*/(.*)$',item,re.M|re.DOTALL)	#get file name
+				if match2:
+					project = match2.group(1)
+					files[i] = match2.group(2)
+				else:
+					files[i] = ""
 		else:
 			print('Error #476 - please replace your CD-ROM')
-	
+
+		#sys.exit("halt!")
 		
 		# post to slack:
 		if (what): #only if they wrote a commit message
-			message = project + ": *" + what + '* (' + file + ')'
+			message = project + ": *" + what + '*' 
+			for s in files:
+				message = message + "\n" + s
+			print(message)
 			slackData = {
 				"attachments": [
 					{
-						"title": who,
+						"title": who + ": " + rev,
 						"text": message,
 						"mrkdwn_in": [
 							"text"
